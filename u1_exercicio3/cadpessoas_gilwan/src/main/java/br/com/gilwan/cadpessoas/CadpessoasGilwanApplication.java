407a39e2b563fde16086e51099e943f0c214bcb4
Gilwan Souza <111159533+GilwanSouza@users.noreply.github.com>
2023-02-25 16:42:10 -0300

    Paginas adicionadas ao projeto

---
+++ u1_exercicio3/cadpessoas_gilwan/src/main/java/br/com/gilwan/cadpessoas/CadpessoasGilwanApplication.java
@@ -0,0 +1,13 @@
+package br.com.gilwan.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasGilwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasGilwanApplication.class, args);
+	}
+
+}
---
+++ u1_exercicio3/cadpessoas_gilwan/src/test/java/br/com/gilwan/cadpessoas/CadpessoasGilwanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gilwan.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasGilwanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
